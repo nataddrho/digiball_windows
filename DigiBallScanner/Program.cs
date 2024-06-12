c13700224f233c0032ec1b8560ed246de20ad354
Nathan Rhoades <Nathan_Rhoades@waters.com>
2024-06-12 13:59:00 -0400

    PNG drawing.

--- DigiBallScanner/Program.cs
+++ DigiBallScanner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.IO;
 using Windows.Devices.Bluetooth;
@@ -8,21 +9,19 @@ using Windows.Storage.Streams;
 using System.Collections.Generic;
 using Windows.Devices.Sensors;
 using Windows.UI.Xaml.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI;
 
 
-public class Variables
-{
-    static int balls = 0;
-}
-
-class Program
+public static class Program
 {   
     public static String filterShortMac = "";
     public static int lastShotNumber = -1;
 
     static async Task Main(string[] args)
     {
-        
+
         String usage = "Usage: xxxxxx \nxxxxxx: Least significant 3 bytes (hex) of DigiBall MAC address.";
         int i = 0;
         foreach (string arg in args)
@@ -67,6 +66,138 @@ class Program
         watcher.Start();
 
         await Task.Delay(Timeout.Infinite);
+    }
+
+    private static void DrawCircle(this Graphics g, Pen pen,
+                                  float centerX, float centerY, float radius)
+    {
+        g.DrawEllipse(pen, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+    }
+
+    private static void FillCircle(this Graphics g, Brush brush,
+                                  float centerX, float centerY, float radius)
+    {
+        g.FillEllipse(brush, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+    }
+
+    private static void drawImage(int angle, int tipPercent)
+    {
+        //Update cueball picture
+
+        int ballDiameter = 345; //Size of blank cueball image in pixels, square
+        int ballRadius = ballDiameter / 2;
+        int tipRadius = 35;
+
+        double ax = Math.Sin(Math.PI / 180 * angle);
+        double ay = -Math.Cos(Math.PI / 180 * angle);
+        double tipEstimationError = 0.15;
+        double tipRadiusDime = 0.358;
+        double tipRadiusCurvatureRatio = tipRadiusDime / 1.125;
+        double est1 = tipPercent * (1 - tipEstimationError) / 100;
+        double est2 = tipPercent * (1 + tipEstimationError) / 100;
+        if (est2 > 0.6) est2 = 0.6;
+        double r1 = ballRadius * est1;
+        double r2 = ballRadius * est2;
+        double drawOffset1 = r1 * tipRadiusCurvatureRatio;
+        double drawOffset2 = r2 * tipRadiusCurvatureRatio;              
+        double px1 = 0;
+        double px2 = 0;
+
+        double s1 = r1 + drawOffset1;
+        if ((s1 - tipRadius) > r2)
+        {
+            px1 = r1 + tipRadius;
+        }
+        else
+        {
+            px1 = s1;
+        }
+        double s2 = r2 + drawOffset2;
+        if ((s2 - tipRadius) > r2)
+        {
+            px2 = r2 + tipRadius;
+        }
+        else
+        {
+            px2 = s2;
+        }
+
+        // Create a Bitmap object from a file.
+        string dir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        string cueball = System.IO.Path.Combine(dir, "blank.png");
+
+        //Bitmap image = new Bitmap(width, height);
+        Bitmap image = new Bitmap(cueball);
+
+        // Create a graphics object from the image
+        Graphics graphics = Graphics.FromImage(image);
+
+        //Tip outline
+        Pen pen = new Pen(System.Drawing.Color.Black);
+        Brush brush = new SolidBrush(System.Drawing.Color.Black);        
+        pen.Width = 2 * tipRadius;
+        float x0 = (float)(ballRadius + px1 * ax);
+        float y0 = (float)(ballRadius + px1 * ay);
+        float x1 = (float)(ballRadius + px2 * ax);
+        float y1 = (float)(ballRadius + px2 * ay);
+        graphics.DrawLine(pen, x0, y0, x1, y1);
+        FillCircle(graphics, brush, x0, y0, tipRadius);
+        FillCircle(graphics, brush, x1, y1, tipRadius);
+
+        // Save the image to a file       
+        image.Save("digiball_tipOutline.png");
+
+        //Grid        
+        pen.Width = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            DrawCircle(graphics, pen, ballRadius, ballRadius, (float)(ballRadius * 0.1 * (i + 1)));
+        }
+        float a = (float)(Math.Sqrt(3) / 2);
+        float b = (float)0.5;
+        graphics.DrawLine(pen, ballRadius, 0, ballRadius, ballDiameter);
+        graphics.DrawLine(pen, 0, ballRadius, ballDiameter, ballRadius);        
+        graphics.DrawLine(pen, ballRadius * (1 + a), ballRadius * (1 + b), ballRadius * (1 - a), ballRadius * (1 - b));
+        graphics.DrawLine(pen, ballRadius * (1 + a), ballRadius * (1 - b), ballRadius * (1 - a), ballRadius * (1 + b));
+        graphics.DrawLine(pen, ballRadius * (1 + b), ballRadius * (1 + a), ballRadius * (1 - b), ballRadius * (1 - a));
+        graphics.DrawLine(pen, ballRadius * (1 + b), ballRadius * (1 - a), ballRadius * (1 - b), ballRadius * (1 + a));
+
+        // Save the image to a file       
+        image.Save("digiball_tipOutlineGrid.png");
+
+        //Tip contact point        
+        pen.Width = 6;
+        pen.Color = System.Drawing.Color.Cyan;
+        brush = new SolidBrush(System.Drawing.Color.Cyan);
+        x0 = (float)(ballRadius + r1 * ax);
+        y0 = (float)(ballRadius + r1 * ay);
+        x1 = (float)(ballRadius + r2 * ax);
+        y1 = (float)(ballRadius + r2 * ay);
+        graphics.DrawLine(pen, x0, y0, x1, y1);
+        FillCircle(graphics, brush, x0, y0, 3);
+        FillCircle(graphics, brush, x1, y1, 3);
+
+        // Save the image to a file       
+        image.Save("digiball_tipOutlineGridContact.png");
+
+        //Guide
+        pen.Color = System.Drawing.Color.Red;
+        pen.Width = 4;
+        graphics.DrawLine(pen, ballRadius, ballRadius, (float)(ballRadius*(1+ax)), (float)(ballRadius*(1+ay)));
+        pen.Width = 6;
+        pen.Color = System.Drawing.Color.Cyan;
+        graphics.DrawLine(pen, x0, y0, x1, y1);
+        FillCircle(graphics, brush, x0, y0, 3);
+        FillCircle(graphics, brush, x1, y1, 3);
+
+        // Save the image to a file               
+        image.Save("digiball_tipOutlineGridContactAngle.png");
+
+        // Dispose of the graphics object and image
+        graphics.Dispose();
+        image.Dispose();
     }
 
     private static async void Watcher_Received(
@@ -114,37 +245,12 @@ class Program
 
                             int angle = Convert.ToInt32(180 / Math.PI * Math.Atan2(spinHorzDPS, spinVertDPS));
 
+                            Console.WriteLine("MAC: {0}, Shot Number: {1}, Angle: {2}, Tip Percent: {3}", shortMac, shotNumber, angle, tipPercent);
+
                             if (dataReady && lastShotNumber!=shotNumber)
                             {                             
                                 lastShotNumber = shotNumber;
-
-                                if (highGAccelAvailable)
-                                {
-                                    Console.WriteLine("MAC: {0}, Shot Number: {1}, Angle: {2}, Tip Percent: {3}", shortMac, shotNumber, angle, tipPercent);
-
-                                    //Update HTML Display               
-                                    string html1 = @"<!DOCTYPE html>
-<html>	
-	<body>
-		<iframe style=""border: none; position: fixed; left: 0; right: 0; top: 0; bottom: 0; width: 100%; height: 100%;"" 
-src=""_digiball_insert.htm?angle=";
-                                    string html2 = @"&tipPercent=";
-                                    string html3 = @"""></iframe>
-
-	</body>
-</html>";
-
-                                    string html = html1 + angle + html2 + tipPercent + html3;
-
-
-                                    File.WriteAllText("display.htm", html);
-
-                                } else
-                                {
-                                    Console.WriteLine("MAC: {0}, Shot Number: {1}, Angle: {2}, Tip Percent: N/A", shortMac, shotNumber, angle);
-                                    Console.WriteLine("Display not updated.");
-                                }
-
+                                drawImage(angle, tipPercent);
                             }
 
 
